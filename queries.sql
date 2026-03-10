b02ef97882fd41dffec91f19476a68a43077eadf
Leonardo Valle <leonardofvalle.01@gmail.com>
2026-03-10 15:15:50 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,9 +1,11 @@
-SELECT stay, 	
-COUNT(*) AS count_int,
-ROUND(AVG(todep),2) AS average_phq,
-ROUND(AVG(tosc),2) AS average_scs,
-ROUND(AVG(toas),2) AS average_as
-	FROM students
-	WHERE inter_dom = 'Inter' AND stay IS NOT NULL
+SELECT 
+	stay, 	
+	COUNT(*) AS count_int,
+	ROUND(AVG(todep),2) AS average_phq,
+	ROUND(AVG(tosc),2) AS average_scs,
+	ROUND(AVG(toas),2) AS average_as
+FROM students
+WHERE inter_dom = 'Inter' 
+AND stay IS NOT NULL
 GROUP BY stay
 ORDER BY stay DESC;
